35442d4feebbe46424103dab3834e85b88e8e1c4
misticslippers <eternal.martyr13579@gmail.com>
2025-12-04 10:41:47 +0300

    rebuild project

--- src/main/java/me/ifmo/backend/Application.java
+++ src/main/java/me/ifmo/backend/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab3InfSysBackendApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lab3InfSysBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
--- src/test/java/me/ifmo/backend/ApplicationTests.java
+++ src/test/java/me/ifmo/backend/ApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lab3InfSysBackendApplicationTests {
+class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
