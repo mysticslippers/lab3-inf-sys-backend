ee8ec9f9ad1d0058b5b70abd9049f50072c46516
misticslippers <eternal.martyr13579@gmail.com>
2025-12-04 10:37:37 +0300

    Initial commit

---
+++ src/main/java/me/ifmo/backend/Lab3InfSysBackendApplication.java
@@ -0,0 +1,13 @@
+package me.ifmo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3InfSysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3InfSysBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ifmo/backend/Lab3InfSysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ifmo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3InfSysBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
