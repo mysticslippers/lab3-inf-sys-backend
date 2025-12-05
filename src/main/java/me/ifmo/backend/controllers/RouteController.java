2198cae3c821dea68b32d44c2ec2f1d9abb5e7fe
misticslippers <eternal.martyr13579@gmail.com>
2025-12-05 18:08:06 +0300

    fix RouteController

--- src/main/java/me/ifmo/backend/controllers/RouteController.java
+++ src/main/java/me/ifmo/backend/controllers/RouteController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/routes")
+@RequestMapping("/routes")
 @RequiredArgsConstructor
 public class RouteController {
 
