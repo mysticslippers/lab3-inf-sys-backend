cfa744432d27d5d0a57b438de90f88070c298ffb
misticslippers <eternal.martyr13579@gmail.com>
2026-02-13 20:01:51 +0300

    add UserServiceImpl

---
+++ src/main/java/me/ifmo/backend/services/impl/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package me.ifmo.backend.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import me.ifmo.backend.entities.User;
+import me.ifmo.backend.repositories.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException("User not found: " + username)
+                );
+
+        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + user.getRole().name());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                List.of(authority)
+        );
+    }
+}
