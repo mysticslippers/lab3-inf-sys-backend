39e18d4aa4981802e13df55ba7e3c7e628f23091
misticslippers <eternal.martyr13579@gmail.com>
2026-02-13 19:51:50 +0300

    add ForgotPasswordRequest

---
+++ src/main/java/me/ifmo/backend/DTO/ForgotPasswordRequest.java
@@ -0,0 +1,17 @@
+package me.ifmo.backend.DTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ForgotPasswordRequest {
+
+    @Email
+    @NotBlank
+    private String email;
+}
